cf29053c23923bc2afae031efa6a6a266a5b5cbd
alumno <alumno@1DAW-027>
2023-02-22 14:09:26 +0100

    parte 6 del ejercicio de entornos

---
+++ src/ejemplojava2023/EjemploJava2023.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplojava2023;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjemploJava2023 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        System.out.println("Epi: ¡Hola Blas!");
+    }
+    
+}
